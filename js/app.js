1c3280e86969cbb82537d48198ceb94a6002c02a
nisariv <112614146+nisariv@users.noreply.github.com>
2022-12-07 22:22:55 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,97 @@
+// Dom7
+var $$ = Dom7;
+
+// Init App
+var app = new Framework7({
+  	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: true
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'AppBase',
+  	routes: [
+		{
+		path: '/inicio/',
+    	url: 'index.html',
+    	name: 'inicio',
+  		},
+		{
+		path: '/grupos/',
+    	url: 'grupos.html',
+    	name: 'grupos',
+  		},
+		{
+		path: '/productos/',
+    	url: 'productos.html',
+    	name: 'productos',
+  		},
+		{
+		path: '/info-producto/',
+    	url: 'info-producto.html',
+    	name: 'info-producto',
+  		},
+		{
+		path: '/perfil/',
+    	url: 'perfil.html',
+    	name: 'perfil',
+  		},
+	],
+	dialog: {
+		title: 'AppBase',
+		buttonOk: 'Aceptar',
+  	},
+	popup: {
+		closeOnEscape: true,
+		backdrop : false
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	}
+});
+
+
+
+$$('#btnLogin').on('click', function (e) {
+	e.preventDefault();
+	
+	var $valid = $$('#form-login')[0].checkValidity();
+	if ($valid){
+        $$('#form-login').trigger('reset');	
+        app.loginScreen.close('#login');
+		
+		app.dialog.alert('!Welcome to AppBase !');
+    }
+	
+});
+
+
+
+$$(document).on('click', '#btnCheckout', function (e) {
+	e.preventDefault();
+
+	
+	var notification = app.notification.create({
+      icon: '<i class="material-icons">check</i>',
+      title: 'Order',
+      titleRightText: '',
+      subtitle: '',
+      text: "Your order has been received.",
+      closeTimeout: 3000,
+    });
+    notification.open();
+	
+});
+
+
+
+
+
